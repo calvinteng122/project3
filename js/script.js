aa5355eee10697d23ae4d63a514bcad5b703a6e2
calvinteng122 <59907179+calvinteng122@users.noreply.github.com>
2020-04-21 00:30:51 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-function init(){
-	var el = document.getElementById('map');
-	var myLocation = new google.maps.Map(documnet, {
-	center: {lat: 41.835117, lng: -87.627130};
-	var mapOptions = {
+function initMap() {
+  var location = {lat: 41.831602, lng: -87.627231};
+  var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
 		mapTypeControlOPtions: {
 			position: google.maps.ControlPosition.Bottom_CENTER
 		});
-	}
+	  var marker = new google.maps.Marker({position: location, map: map});
 }
-	
 	
 google.mpas.event.addDomListener(window, 'load', init);
