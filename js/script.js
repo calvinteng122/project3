8010a0a187361e130e4c06ca18a67db791438386
calvinteng122 <59907179+calvinteng122@users.noreply.github.com>
2020-04-21 14:24:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 41.831602, lng: -87.627231};
-		zoom: 8
+function init(){
+	var el = document.getElementById('map');
+	var myLocation = new google.maps.LatLng(41.880858, -87.674069);
+	var mapOptions = {
+		center: myLocation,
+		zoom: 18,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions:{
-			position: google.maps.ControlPosition.BOttom_CENTER
+		mapTypeControlOptions: {
+			position: google.maps.ControlPosition.BOTTOM_CENTER
 		}
-		};
+	};
+
+	var myMap = new google.maps.Map(el, mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: myLocation,
+		map: myMap,
+		animation: google.maps.Animation.BOUNCE,
+		icon: 'images/chicago-teams.jpg'
+	});
+
+	var contentString = '<h1>The United Center</h1><p>is home to the 6-time champions, the Chicago Bulls and the Chicago Blackhawks .</p>';
+
+	var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  	});
+
+	google.maps.event.addListener(marker, 'mouseover', function() {
+    	infowindow.open(myMap, marker);
+  	});
 }
-	
-google.mpas.event.addDomListener(window, 'load', init);
+
+google.maps.event.addDomListener(window, 'load', init);
